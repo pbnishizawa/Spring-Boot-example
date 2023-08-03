541f4b4fc46856c30f504a1f88909aca38a4d475
pbnishizawa <nishizawa@primebrains.co.jp>
2023-08-03 19:27:04 +0900

    Aug 3rd

--- src/main/java/com/example/demo/controller/HelloWorld.java
+++ src/main/java/com/example/demo/controller/HelloWorld.java
@@ -1,12 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//localhost:8080?name=nishizawa
 public class HelloWorld {
     @GetMapping("/hello")
-    public String getHello() {
-        return "Hello, World";
+    public String getHello(@RequestParam("name") String name) {
+        return "Hello, " + name;
     }
 }
